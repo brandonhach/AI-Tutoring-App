573aa287c80dad4ed2ded47fa67337e5ea633f0e
Sean-Patrick <rowan.seanp@gmail.com>
2023-06-29 11:56:37 -0400

    deleted react files to start refactoring

--- chatbox/src/Components/About/About.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./about.css";
-
-const About = () => {
-	return <div>About</div>;
-};
-
-export default About;
--- chatbox/src/Components/Chatbox/Chatbox.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./chatbox.css";
-
-const ChatBox = () => {
-	return <div>ChatBox</div>;
-};
-
-export default ChatBox;
