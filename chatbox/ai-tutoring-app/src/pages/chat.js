d71e0511fb6449d881c162e141dc370790ec7ddf
brandonhach <specialcambo@gmail.com>
2023-07-06 09:58:41 -0400

    Added comments

--- chatbox/ai-tutoring-app/src/pages/chat.js
+++ chatbox/ai-tutoring-app/src/pages/chat.js
@@ -4,10 +4,22 @@ import Nav from "../../components/nav";
 import { useState } from "react";
 
 export default function Chat() {
+	/**
+	 * 	useState for holding prompt from user and response from bot
+	 */
 	const [chat, setChat] = useState("");
 	const [botResponse, setBotResponse] = useState("");
 
+	/**
+	 * Async function responsible for sending a GET request to our custom API.
+	 * The purpose is to interact with a chatbot by sending the user's prompt to
+	 * recieve a response from the bot.
+	 */
 	const sendToserver = async () => {
+		/**
+		 * Send a GET request to the chatbot endpoint.
+		 * Chat is appended as "prompt" in the URL.
+		 */
 		const response = await fetch("http://localhost:8080/bot/chat?prompt=" + chat, {
 			method: "Get",
 			headers: {
@@ -15,8 +27,17 @@ export default function Chat() {
 			},
 		});
 
+		/**
+		 * botResponse is received as text.
+		 * Then updates the botResponse state with the chatbot's response.
+		 */
 		const botResponse = await response.text();
 		setBotResponse(botResponse);
+
+		/**
+		 * Stores the current message using Arrow function.
+		 * To log responses or prompt, invoked logic into springboot.
+		 */
 		setChat((prevChat) => prevChat + "\n\nBot: " + botResponse);
 	};
 
